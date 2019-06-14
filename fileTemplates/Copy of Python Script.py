37cd99e3a125724c5d6902c4ff85183db9b432f3
Zhipeng Han <hanzhipeng9@gmail.com>
2019-06-14 16:23:00 +0800

    PC-2019.1.3 <Warmer@Warmers-iMac.local Create markdown.xml, vim_settings.xml, project.default.xml, diff.xml, Default.xml, Sublime.xml, file.template.settings.xml, find.xml, ui.lnf.xml, log_highlighting.xml, filetypes.xml, Python Script.py, colors.scheme.xml, notifications.xml, _@user_Darcula.icls, gutter.xml, Default.xml, jdk.table.xml, intentionSettings.xml, laf.xml, Python.xml, editor.codeinsight.xml, Copy of Python Script.py, keymap.xml, editor.xml, applicationLibraries.xml, debugger.xml, Mac OS X copy.xml, updates.xml

---
+++ fileTemplates/Copy of Python Script.py
@@ -0,0 +1,15 @@
+# -----------------------------------------------------
+# Copyright (c) ${YEAR} Apple Inc. All rights reserved.
+#
+# Created by ${USER} on ${DATE}.
+# -----------------------------------------------------
+"""
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import logging
+
+
+logger = logging.getLogger(__name__)
